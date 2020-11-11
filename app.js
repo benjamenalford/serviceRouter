2c39a8ed213916abf8b7e3ba9877be6da8a5baca
Benjamen Alford <benjamen.alford@gmail.com>
2020-11-11 02:06:31 -0600

    fleshing the app out

--- app.js
+++ app.js
@@ -2,20 +2,20 @@ const express = require('express')
 const bodyParser = require('body-parser');
 
 const app = express()
-const port = 3000
+const port = 8001
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 //app.engine('.html', whiskers.__express);
 
-app.get('/', (req, res) => {
+app.get('/api/', (req, res) => {
     res.send([{ "id": 1 }]);
 })
 
-app.post('/', (req, res) => {
+app.post('/api/', (req, res) => {
     //console.log('Got body:', req.body);
     //console.log(req.body["results"][0]["id"])
     res.sendStatus(200);
 })
 app.listen(port, () => {
-    console.log(`Example app listening at http: //localhost:${port}`)
+    console.log(`server loaded at ${port}`)
 })
---
+++ logger.js
@@ -0,0 +1,22 @@
+const bunyan = require('bunyan');
+const config = require('./config').config;
+
+//var log = bunyan.createLogger({ name: 'SeeClickFixLogger' });
+exports.log = bunyan.createLogger({
+    name: 'SeeClickFixLogger',
+    streams: [{
+            level: 'info',
+            stream: process.stdout,
+        },
+        {
+            level: 'error',
+            //  stream: process.stdout,
+        },
+        {
+            level: 'debug',
+            // stream: process.stdout,
+            path: './logs/seeClick-debug.log'
+        }
+    ],
+    level: 'info'
+});
