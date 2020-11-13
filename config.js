3d586337234d4f98efcd23dde56a8c041479a5c1
Benjamen Alford <benjamen.alford@gmail.com>
2020-11-13 09:39:40 -0600

    pushed the config

---
+++ config.js
@@ -0,0 +1,18 @@
+// config
+//TODO - load from external files and only show methods here
+exports.config = {
+    appName: "serviceRouter",
+    db: {
+        name: 'mongo',
+        url: 'mongodb://192.168.1.10:27017',
+        dbName: 'ephemera',
+        collectionName: "logger"
+    },
+    apis: {
+        //TODO make up mind
+    },
+    http: {
+        port: 3000
+    },
+    debug: false
+}
