372c8ee9d79aab8e1139468c16ee2e98b7522ae2
Benjamen Alford <benjamen.alford@gmail.com>
2020-12-09 00:21:48 -0600

    better routing dao

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express')
 const bodyParser = require('body-parser');
 const config = require('./config').config;
 const logger = require('./logger').log;
-const dao = require('./dao');
+const dao = require('./dao').dao;
 const app = express()
 const port = config.http;
 
@@ -11,18 +11,19 @@ app.use(bodyParser.json());
 //app.engine('.html', whiskers.__express);
 
 app.get('/api/', (req, res) => {
-    res.send([{ "id": 1 }]);
-})
+    dao.read({}, res)
+});
 
-app.post('/api/', (req, res) => {
-    //console.log('Got body:', req.body);
-    //console.log(req.body["results"][0]["id"])
-    res.sendStatus(200);
-})
+app.get('/api/:collection', (req, res) => {
+    dao.read({}, res)
+});
+// app.post('/api/', (req, res) => {
+//     //console.log('Got body:', req.body);
+//     //console.log(req.body["results"][0]["id"])
+//     res.sendStatus(200);
+// })
+
 app.listen(port, () => {
-    console.log(`server loaded at ${port}`);
+    console.log(`server loaded at ${port.port}`);
     logger.info("started");
-    dao.dao.write({ id: 1 });
-    var x = dao.dao.read("test");
-    console.log(x)
 })
--- dao.js
+++ dao.js
@@ -3,15 +3,14 @@ const config = require('./config').config;
 const logger = require('./logger').log;
 
 exports.dao = {
-    write: function(d) {
-        console.log("write " + d + config.db.collectionName + config.db.url);
+    write: (d) => {
+        console.log(`write ${d}${config.db.collectionName}${config.db.url}`);
         MongoClient.connect(config.db.url, function(err, db) {
             if (err) {
                 logger.error(err);
                 throw err;
             };
             var dbo = db.db(config.db.dbName);
-            var query = {};
             dbo.collection(config.db.collectionName).insert(d, (err, d) => {
                 if (err) {
                     logger.error(err);
@@ -20,33 +19,37 @@ exports.dao = {
             });
         });
     },
-    read: function(query) {
-        console.log("read " + query);
-        var response;
-        MongoClient.connect(config.db.url, function(err, db) {
-            if (err) {
-                logger.error(err);
-                throw err;
-            };
-            var dbo = db.db(config.db.dbName);
-            query = {};
-
-            dbo.collection(config.db.collectionName).find(query).toArray(function(err, result) {
+    read: (query, res) => {
+        try {
+            MongoClient.connect(config.db.url, (err, db) => {
                 if (err) {
                     logger.error(err);
                     throw err;
-                }
-                db.close();
+                };
+                let dbo = db.db(config.db.dbName);
 
+                let daoPromise = () => {
+                    return new Promise((resolve, reject) => {
+                        db(config.db.dbName).collection(config.db.collectionName)
+                            .find(query)
+                            .toArray(function(err, data) {
+                                err ? reject(err) : resolve(data);
+                            });
+                    });
+                };
+
+                let callDaoPromise = async() => {
+                    return await (daoPromise());
+                };
+
+                callDaoPromise().then(function(result) {
+                    dbo.close();
+                    res.json(result);
+                });
             });
-
-            // dbo.collection(config.db.collectionName).find().toArray().then(function(err, data) {
-            //     err ?
-            //         reject(err) :
-            //         resolve(data);
-            // });
-
-        });
-        return response;
+        } catch (err) {
+            logger.error(err);
+            next(err);
+        }
     }
 }
